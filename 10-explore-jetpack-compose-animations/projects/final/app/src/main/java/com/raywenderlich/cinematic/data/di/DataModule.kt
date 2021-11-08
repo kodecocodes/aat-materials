35c209cb1f1fbff91935b6f84546bc36aacae9f1
Prateek Prasad <leavethemails@gmail.com>
2021-11-08 09:41:16 +0530

    Add chapter 10 projects

---
+++ 10-explore-jetpack-compose-animations/projects/final/app/src/main/java/com/raywenderlich/cinematic/data/di/DataModule.kt
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2021 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * This project and source code may use libraries or frameworks that are
+ * released under various Open-Source licenses. Use of those libraries and
+ * frameworks are governed by their own individual licenses.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.raywenderlich.cinematic.data.di
+
+import android.app.Application
+import androidx.room.Room
+import com.raywenderlich.cinematic.data.MoviesDataRepository
+import com.raywenderlich.cinematic.data.repository.MoviesRepository
+import com.raywenderlich.cinematic.data.sources.CastDao
+import com.raywenderlich.cinematic.data.sources.MoviesCache
+import com.raywenderlich.cinematic.data.sources.MoviesDao
+import com.raywenderlich.cinematic.data.sources.MoviesDatabase
+import com.raywenderlich.cinematic.data.store.MovieCacheStore
+import org.koin.android.ext.koin.androidApplication
+import org.koin.dsl.module
+
+val dataModule = module {
+
+  fun provideDatabase(application: Application): MoviesDatabase {
+    return Room.databaseBuilder(application, MoviesDatabase::class.java, "movies.db")
+        .fallbackToDestructiveMigration()
+        .build()
+  }
+
+  fun provideMoviesDao(database: MoviesDatabase): MoviesDao {
+    return database.moviesDao
+  }
+
+  fun provideCastDao(database: MoviesDatabase): CastDao {
+    return database.castDao
+  }
+
+  single { provideDatabase(androidApplication()) }
+
+  single { provideMoviesDao(get()) }
+
+  single { provideCastDao(get()) }
+
+  factory<MoviesCache> { MovieCacheStore(get(), get()) }
+
+  factory<MoviesRepository> { MoviesDataRepository(get()) }
+}
